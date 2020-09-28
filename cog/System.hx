75687408483417ef92b089788323e0bedca93fe7
austin <eastin93@gmail.com>
2020-09-28 16:35:59 -0400

    add fixed timestep, timescale, and active toggle

--- cog/System.hx
+++ cog/System.hx
@@ -2,7 +2,19 @@ package cog;
 
 @:autoBuild(cog.Macros.build_system())
 class System {
+  public var active:Bool = true;
+
+  public var time_scale:Float = 1;
+
+  public var fixed:Bool = false;
+
+  public var fixed_framerate(default, set):Float = 60;
+
   var engine:Engine;
+
+  var fixed_accumulator:Float = 0;
+
+  var fixed_dt:Float;
 
   public function new() {}
 
@@ -18,7 +30,26 @@ class System {
     remove_nodes();
   }
 
+  @:allow(cog.Engine)
+  function try_step(dt:Float) {
+    if (!active) return;
+    if (fixed) {
+      fixed_accumulator += dt;
+      while (fixed_accumulator > fixed_dt) {
+        fixed_accumulator -= fixed_dt;
+        step(fixed_dt * time_scale);
+      }
+    }
+    else step(dt * time_scale);
+  }
+
   function add_nodes() {}
 
   function remove_nodes() {}
+
+  function set_fixed_framerate(v:Float) {
+    fixed_framerate = Math.max(v, 0);
+    fixed_dt = 1 / fixed_framerate;
+    return fixed_framerate;
+  }
 }
